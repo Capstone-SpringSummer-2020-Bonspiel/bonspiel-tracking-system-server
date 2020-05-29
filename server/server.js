0ce9f65649803b3efa8af05ec3c399a2cc3e4aec
David Kim <kim.david.j@gmail.com>
2020-05-28 17:04:51 -0700

    Added 2 routes for curling events

--- server/server.js
+++ server/server.js
@@ -10,6 +10,50 @@ const config = require('config');
 
 const app = express();
 
+/****************************************************************************/
+
+// Setup postgres db connection
+const { Pool, Client } = require('pg');
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: 'postgres',
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+// Fetch curling events
+app.get('/api/fetch-curling-events', (req, res) => {
+  pool.query('SELECT * FROM public.curlingevent ORDER BY id ASC', (err, _res) => {
+    console.log(err, _res);
+    res.send(_res);
+  });
+});
+
+// Create a curling event
+app.post('/api/create-curling-event', (req, res) => {
+  pool.query(`
+    INSERT INTO public.curlingevent (
+      name,
+      event_type,
+      info,
+      completed,
+      begin_date,
+      end_date)
+    VALUES (
+      '${req.body.name}',
+      '${req.body.eventType}',
+      '${req.body.info}',
+      '${req.body.completed}',
+      '${req.body.beginDate}',
+      '${req.body.endDate}')`, (err, _res) => {
+    console.log(err, _res);
+    res.send(_res);
+  });
+});
+
+/****************************************************************************/
+
 app.all('*', (req, res) => {
   res.send('Hello World!');
 })
@@ -17,4 +61,4 @@ app.all('*', (req, res) => {
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 console.log("Backend Environment", config.env);
-console.log("Backend URl", config.backend.url);
+console.log("Backend URL", config.backend.url);
