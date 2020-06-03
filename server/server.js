7ed567b15eaa6639731879363ac492c6b8467ce6
Tejas Menon <tejas.menon@hotmail.com>
2020-06-02 20:24:39 -0700

    debugging #11

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 let swaggerConfig = loadSwaggerConfig();
 
-swaggerConfig.host = config.backend.url;
+swaggerConfig.host = servers.url;
 fs.writeFileSync('./config/swagger.yaml', YAML.safeDump(swaggerConfig), 'utf8');
 
 swaggerConfig = loadSwaggerConfig(); //reload for updated swagger
