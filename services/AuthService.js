b4a7ce6f702d32f139c05886741f163c93ed8b27
Tejas Menon <tejas.menon@hotmail.com>
2020-07-03 13:41:42 -0700

    Revert "Merge pull request #17 from Capstone-SpringSummer-2020-Bonspiel/revert-15-BP-97_Api-Auth"
    This reverts commit 6e0e295bce6525a88fec392dd4d59507cd7a9f23, reversing
    changes made to 665d06eaab3b43a8991c61c5433bd7472c3cc60e.

---
+++ services/AuthService.js
@@ -0,0 +1,91 @@
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const jwtKey = "privateKey"; // config.jwtKey
+const jwtExpirySeconds = 600 // 10 minutes
+
+const users = {
+  admin: "password"
+} // Can store in db later
+
+function generateSalt(length) {
+  return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+}
+
+function hash(password, salt) {
+
+}
+
+class AuthService {
+  signIn(req, res) {
+    const { username, password } = req.body;
+    if (!username || !password || users[username] !== password) {
+      return res.status(401).end();
+    }
+
+    const token = jwt.sign({ username }, jwtKey, {
+      algorithm: "HS256",
+      expiresIn: jwtExpirySeconds
+    })
+
+    console.log(token);
+
+    // Max age is in milliseconds.
+    res.cookie("token", token, { maxAge: jwtExpirySeconds * 1000 })
+    res.end();
+  }
+
+  authorize(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).end();
+    }
+
+    var payload;
+    try {
+      payload = jwt.verify(token, jwtKey);
+    }
+    catch (err) {
+      if (err instanceof jwt.JsonWebTokenError) {
+        return res.status(401).end();
+      }
+
+      return res.status(400).end();
+    }
+
+    next();
+  }
+
+  refresh(req, res) {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).end();
+    }
+    var payload;
+    try {
+      payload = jwt.verify(token, jwtKey);
+    }
+    catch (err) {
+      if (err instanceof jwt.JsonWebTokenError) {
+        return res.status(401).end();
+      }
+
+      return res.status(400).end();
+    }
+
+    const currentUnixSeconds = Math.round(Number(new Date()) / 1000);
+    const expiryThreshold = 30; // seconds
+    if (payload.exp - currentUnixSeconds > expiryThreshold) {
+      return res.status(400).end();
+    }
+
+    const newToken = jwt.sign({ username: payload.username }, jwtKey, {
+      algorithm: "HS256",
+      expiresIn: jwtExpirySeconds
+    })
+    res.cookie("token", newToken, { maxAge: jwtExpirySeconds * 1000 });
+    res.end();
+  }
+}
+
+module.exports = AuthService;
