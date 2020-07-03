2fd0a8794c083b16069ce900232c075132cbd322
Tejas Menon <tejas.menon@hotmail.com>
2020-07-03 12:37:29 -0700

    Revert "Add AuthService"

--- server/server.js
+++ server/server.js
@@ -5,10 +5,8 @@ const swaggerUI = require('swagger-ui-express');
 const YAML = require('js-yaml');
 const fs = require('fs');
 const routes = require('../routes/routes');
-const adminRoutes = require('../routes/adminRoutes');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
@@ -23,12 +21,10 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(bodyParser.json());
-app.use(cookieParser())
 
 let swaggerConfig = loadSwaggerConfig();
 
 swaggerConfig.servers[0].url = config.backend.url + "api/v1";
-swaggerConfig.servers[1].url = config.backend.url + "api/v1/admin";
 fs.writeFileSync('./config/swagger.yaml', YAML.safeDump(swaggerConfig), 'utf8');
 
 swaggerConfig = loadSwaggerConfig(); //reload for updated swagger
@@ -43,7 +39,6 @@ function loadSwaggerConfig() {
 console.debug("baseUrl", swaggerConfig.host);
 
 app.use('/api/v1/', routes);
-app.use('/api/v1/admin', adminRoutes);
 app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 app.listen(PORT, HOST);
