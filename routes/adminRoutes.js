4a9746e92efcebf76a0ea4d2201edb822c540c12
Tejas Menon <tejas.menon@hotmail.com>
2020-07-04 12:00:52 -0700

    change server path

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const AuthService = require('../services/AuthService');
 const authService = new AuthService();
-const curlingEventService = require('../server/server');
+const curlingEventService = require('../server');
 
 router.post('/signIn', authService.signIn);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const curlingEventService = require('../server/server');
+const curlingEventService = require('../server');
 
 router.get('/events/:curlingEventId/teams/:teamId/games', async (req, res) => {
   const curlingEventId = req.params.curlingEventId;
