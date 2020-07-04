96711b3be43b953ec65bcc219896c2ecdd639e0b
Tejas Menon <tejas.menon@hotmail.com>
2020-07-04 00:32:56 -0700

    remove newlines

--- services/Exceptions.js
+++ services/Exceptions.js
@@ -2,11 +2,15 @@
 class Exceptions {
 
   deleteDrawException = (techDesc) => {
-    return `Failed to delete draw.
+    return removeLineBreaks(
+      `Failed to delete draw.
      Draw may contain games in bracket event connected to other games. 
      Make sure to delete draws from oldest to newest.
-     Technical Description: ${techDesc}`;
+     Technical Description: ${techDesc}`
+    );
   };
+
+  removeLineBreaks = (sentence) => sentence.replace(/(\r\n|\n|\r)/gm, "");
 }
 
 module.exports = Exceptions;
