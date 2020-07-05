608c551c5f6ca39628e1d8784ee1e711d51f7392
Aaron <aaron@desktop>
2020-07-05 16:02:34 -0700

    Revert expiryThreshold

--- services/AuthService.js
+++ services/AuthService.js
@@ -135,7 +135,7 @@ class AuthService {
     }
 
     const currentUnixSeconds = Math.round(Number(new Date()) / 1000);
-    const expiryThreshold = 120; // seconds
+    const expiryThreshold = 30; // seconds
     if (payload.exp - currentUnixSeconds > expiryThreshold) {
       return res.status(400).end();
     }
