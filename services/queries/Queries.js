85708bc3cedf1a014fc19caa5c5095d546b0a375
AaronCs <AaronCs@users.noreply.github.com>
2020-07-05 16:14:43 -0700

    Fix GET Routes (#20)
    * Add deleteDraw route
    
    * test with couple pools
    
    * debug #1
    
    * debug #2
    
    * Bad request error
    
    * Stringify error
    
    * debug #3
    
    * remove newlines
    
    * debug #4
    
    * user import syntax
    
    * revert back to require syntax
    
    * change server path
    
    * use object literal
    
    * ES6 import
    
    * use instance specific export
    
    * remove circular dependency
    
    * change syntax
    
    * fix export bug
    
    * Add deleteTeam route
    
    * add deleteCurler
    
    * debug #2
    
    * add deleteOrg Route
    
    * Add nullIdException
    
    * inject pool and add refresh route
    
    * add expiry seconds of webtoken to config file
    
    * add better exception description
    
    * edit Exceptions better
    
    * better edited exceptions #2
    
    * add deletePool and deleteBracket
    
    * debug #5
    
    * add nullChecker
    
    * add removeTeamFromEvent
    
    * debug #4
    
    * debug await
    
    * add deleteGame
    
    * add deleteEnd
    
    * Fix GET games and scores, ExpiryThreshold change
    
    * Threshold change for testing
    
    * Threshold test
    
    * Revert expiryThreshold
    
    Co-authored-by: Tejas Menon <tejas.menon@hotmail.com>
    Co-authored-by: Aaron <aaron@desktop>
    Co-authored-by: Tejas Menon <tejas2@pdx.edu>

--- services/queries/Queries.js
+++ services/queries/Queries.js
@@ -22,35 +22,25 @@ FULL JOIN curler on curler.curlingteam_id=curlingteam.id
 WHERE curlingteam.id=$1;
 `;
 
-/*
- * @param curlingEventId, curlingTeamId
- */
-const GET_ALL_GAMES_BY_TEAM_IN_CURLING_EVENT = `
-SELECT curlingteam.name as team_name, game.*
-FROM public.game
-JOIN public.curlingteam ON game.curlingteam1_id=curlingteam.id
-JOIN public.eventteams ON eventteams.team_id=curlingteam.id
-JOIN public.curlingevent ON curlingevent.id=eventteams.event_id
-WHERE curlingteam.id=$2
-AND curlingevent.id=$1
-UNION
-SELECT curlingteam.name as team_name, game.*
-FROM public.game
-JOIN public.curlingteam ON game.curlingteam2_id=curlingteam.id
-JOIN public.eventteams ON eventteams.team_id=curlingteam.id
-JOIN public.curlingevent ON curlingevent.id=eventteams.event_id
-WHERE curlingteam.id=$2
-AND curlingevent.id=$1;
-`;
-
 const GET_ALL_GAMES_IN_CURLING_EVENT = `
-SELECT curlingteam1.name as team_name1, curlingteam2.name as team_name2, game.*
-FROM public.game
-JOIN public.curlingteam as curlingteam1 ON game.curlingteam1_id=curlingteam1.id
-JOIN public.curlingteam as curlingteam2 ON game.curlingteam2_id=curlingteam2.id
-JOIN public.draw ON game.draw_id=draw.id
+SELECT curlingteam1.name as team_name1, curlingteam2.name as team_name2,
+game.id as game_id, game.event_type, game.notes, game.bracket_id, game.pool_id, game.draw_id, game.curlingteam1_id, game.curlingteam2_id, game.stone_color1, game.stone_color2, game.winner_dest, game.loser_dest, game.ice_sheet, game.finished, game.winner, 
+FROM game
+JOIN curlingteam as curlingteam1 ON game.curlingteam1_id=curlingteam1.id
+JOIN curlingteam as curlingteam2 ON game.curlingteam2_id=curlingteam2.id
+JOIN draw ON game.draw_id=draw.id
 WHERE draw.event_id=$1
 ORDER BY game.id;`;
+
+const GET_ALL_GAMES_BY_TEAM = `
+SELECT curlingteam1.name as team_name1, curlingteam2.name as team_name2,
+game.id as game_id, game.event_type, game.notes, game.bracket_id, game.pool_id, game.draw_id, game.curlingteam1_id, game.curlingteam2_id, game.stone_color1, game.stone_color2, game.winner_dest, game.loser_dest, game.ice_sheet, game.finished, game.winner 
+FROM game
+JOIN curlingteam as curlingteam1 ON game.curlingteam1_id=curlingteam1.id
+JOIN curlingteam as curlingteam2 ON game.curlingteam2_id=curlingteam2.id
+JOIN draw ON game.draw_id=draw.id
+WHERE draw.event_id=$1
+AND (curlingteam1_id=$2 OR curlingteam2_id=$2);`;
 
 const GET_ALL_DRAWS_IN_CURLING_EVENT = `
 SELECT draw.*
@@ -60,41 +50,83 @@ WHERE curlingevent.id=$1
 `;
 
 const GET_ALL_GAMES_AND_SCORES = `
-SELECT curlingteam.name, game.*, endscore.*
-FROM public.game
-JOIN public.curlingteam ON game.curlingteam1_id=curlingteam.id
-JOIN public.eventteams ON eventteams.team_id=curlingteam.id
-JOIN public.curlingevent ON curlingevent.id=eventteams.event_id
-JOIN public.endscore ON game.id=endscore.game_id
-WHERE curlingevent.id=$1
-UNION
-SELECT curlingteam.name, game.*, endscore.*
-FROM public.game
-JOIN public.curlingteam ON game.curlingteam2_id=curlingteam.id
-JOIN public.eventteams ON eventteams.team_id=curlingteam.id
-JOIN public.curlingevent ON curlingevent.id=eventteams.event_id
-JOIN public.endscore ON game.id=endscore.game_id
-WHERE curlingevent.id=$1;
+SELECT 
+curlingteam1.name as team_name1, curlingteam2.name as team_name2, 
+game.id as game_id, game.event_type, game.notes, game.bracket_id, game.pool_id, game.draw_id, game.curlingteam1_id, game.curlingteam2_id, game.stone_color1, game.stone_color2, game.winner_dest, game.loser_dest, game.ice_sheet, game.finished, game.winner, 
+endscore.id as endscore_id, endscore.end_number, endscore.curlingteam1_scored, endscore.score, endscore.blank
+FROM game
+JOIN curlingteam as curlingteam1 ON game.curlingteam1_id=curlingteam1.id
+JOIN curlingteam as curlingteam2 ON game.curlingteam2_id=curlingteam2.id
+JOIN draw ON game.draw_id=draw.id
+LEFT OUTER JOIN endscore ON endscore.game_id=game.id
+WHERE draw.event_id=$1
+ORDER BY curlingteam1_id;
 `;
 
-const GET_ALL_GAMES_AND_SCORES_BY_TEAM_IN_CURLING_EVENT = `
-SELECT curlingteam.name, game.*, endscore.*
+const GET_ALL_GAMES_AND_SCORES_BY_TEAM = `
+SELECT 
+curlingteam1.name as team_name1, curlingteam2.name as team_name2, 
+game.id as game_id, game.event_type, game.notes, game.bracket_id, game.pool_id, game.draw_id, game.curlingteam1_id, game.curlingteam2_id, game.stone_color1, game.stone_color2, game.winner_dest, game.loser_dest, game.ice_sheet, game.finished, game.winner, 
+endscore.id as endscore_id, endscore.end_number, endscore.curlingteam1_scored, endscore.score, endscore.blank
+FROM game
+JOIN curlingteam as curlingteam1 ON game.curlingteam1_id=curlingteam1.id
+JOIN curlingteam as curlingteam2 ON game.curlingteam2_id=curlingteam2.id
+JOIN draw ON game.draw_id=draw.id
+LEFT OUTER JOIN endscore ON endscore.game_id=game.id
+WHERE draw.event_id=$1
+AND (curlingteam1_id=$2 OR curlingteam2_id=$2);
+`;
+
+
+const GET_GAMES_PLAYED_BY_TEAM_IN_EVENT = `
+SELECT *
 FROM public.game
-JOIN public.curlingteam ON game.curlingteam1_id=curlingteam.id
-JOIN public.eventteams ON eventteams.team_id=curlingteam.id
-JOIN public.curlingevent ON curlingevent.id=eventteams.event_id
-JOIN public.endscore ON game.id=endscore.game_id
-WHERE curlingevent.id=$1
-AND curlingteam.id=$2
-UNION
-SELECT curlingteam.name, game.*, endscore.*
-FROM public.game
-JOIN public.curlingteam ON game.curlingteam2_id=curlingteam.id
-JOIN public.eventteams ON eventteams.team_id=curlingteam.id
-JOIN public.curlingevent ON curlingevent.id=eventteams.event_id
-JOIN public.endscore ON game.id=endscore.game_id
-WHERE curlingevent.id=$1
-AND curlingteam.id=$2;
+JOIN public.draw ON draw.id=game.draw_id
+WHERE draw.event_id=$1
+AND (game.curlingteam2_id=$2 OR game.curlingteam1_id=$2)
+`;
+
+const DELETE_DRAW = `
+DELETE FROM draw WHERE id=$1
+`;
+
+const DELETE_TEAM = `
+DELETE FROM curlingteam WHERE id=$1
+`;
+
+const DELETE_CURLER = `
+DELETE FROM curler WHERE id=$1
+`;
+
+const DELETE_ORG = `
+DELETE FROM organization WHERE id=$1
+`;
+
+const DELETE_POOL = `
+DELETE FROM pool WHERE id=$1
+`;
+
+const DELETE_BRACKET = `
+DELETE FROM bracket WHERE id=$1
+`;
+
+const DELETE_TEAM_IN_EVENT = `
+DELETE FROM eventteams WHERE event_id=$1 AND team_id=$2
+`;
+
+const DELETE_GAME = `
+DELETE FROM game WHERE id=$1
+`;
+
+const DELETE_END = `
+DELETE FROM endscore WHERE id=$1
+`;
+
+const GET_GAME_FROM_END_ID = `
+SELECT * 
+FROM endscore
+JOIN game ON endscore.game_id=game.id
+WHERE endscore.id=$1
 `;
 
 const GET_GAMES_PLAYED_BY_TEAM_IN_EVENT = `
@@ -161,11 +193,11 @@ VALUES ($1, $2, $3, $4);
 
 module.exports = {
   GET_ALL_CURLERS,
-  GET_ALL_GAMES_IN_CURLING_EVENT,
-  GET_ALL_GAMES_BY_TEAM_IN_CURLING_EVENT,
   GET_ALL_DRAWS_IN_CURLING_EVENT,
+  GET_ALL_GAMES_IN_CURLING_EVENT,
+  GET_ALL_GAMES_BY_TEAM,
   GET_ALL_GAMES_AND_SCORES,
-  GET_ALL_GAMES_AND_SCORES_BY_TEAM_IN_CURLING_EVENT,
+  GET_ALL_GAMES_AND_SCORES_BY_TEAM,
   GET_ALL_TEAMS_IN_CURLING_EVENT,
   GET_CURLING_TEAM,
   GET_GAMES_PLAYED_BY_TEAM_IN_EVENT,
