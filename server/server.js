edc476b73a16b775ab23a54861229fa24d1a5862
Tejas Menon <tejas.menon@hotmail.com>
2020-07-10 18:27:44 -0700

    localhost

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const config = require('config');
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost", "http://herokuapp.com"],
+  origin: '/localhost|heroku',
   credentials: true
 }));
 
