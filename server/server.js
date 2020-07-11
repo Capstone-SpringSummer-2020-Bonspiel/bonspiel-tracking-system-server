c0550ab47d126bd2199c783de59201f575952351
Tejas Menon <tejas.menon@hotmail.com>
2020-07-10 19:45:53 -0700

    check allowedHeaders

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: ['https://bonspiel-tracking-system-devl.herokuapp.com', 'https://bonspiel-tracking-system-devl.herokuapp.com/admin'],
   credentials: true,
-  allowedHeaders: ['content-type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  allowedHeaders: ['content-type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 }));
 
